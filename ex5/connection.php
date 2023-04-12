2144467fcbf14ed6d009fe6f4766b8e5644117b0
ajefi <aljefi@taltech.ee>
2023-04-12 16:17:53 +0300

    ex5

---
+++ ex5/connection.php
@@ -0,0 +1,13 @@
+<?php
+
+const USERNAME = 'ajveeb';
+const PASSWORD = 'a7d742';
+
+function getConnection(): PDO {
+    $host = 'db.mkalmo.eu';
+
+    $address = sprintf('mysql:host=%s;port=3306;dbname=%s',
+        $host, USERNAME);
+
+    return new PDO($address, USERNAME, PASSWORD);
+}
